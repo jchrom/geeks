82d00297a509ed05bfb21bb15c7ba24d3c3fa733
Jakub Chromec <chromec.jakub@gmail.com>
2020-09-12 13:21:04 +0100

    document package

---
+++ R/geeks.R
@@ -0,0 +1,20 @@
+#' geeks: Geek API client
+#'
+#' The purpose of `geeks` is to fetch data from three famous geek websites
+#' using [API2](https://boardgamegeek.com/wiki/page/BGG_XML_API2).
+#'
+#' Geek websites:
+#'
+#' * https://www.boardgamegeek.com
+#' * https://rpggeek.com
+#' * https://videogamegeek.com
+#'
+#' @importFrom magrittr %>%
+#'
+#' @importFrom xml2 xml_child xml_attr xml_text xml_find_first xml_find_all
+#'   xml_contents xml_length xml_attrs xml_children xml_has_attr
+#'
+#' @docType package
+#' @name geeks
+NULL
+#> NULL
